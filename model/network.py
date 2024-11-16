d9cb05561ba2079d4f9d39862ec02e52e1bdb479
chbsaikiran <sai.406@gmail.com>
2024-11-16 17:33:53 +0530

    Now the all tests are passing the number of parameters is less than 25000

--- model/network.py
+++ model/network.py
@@ -4,10 +4,10 @@ import torch.nn as nn
 class SimpleCNN(nn.Module):
     def __init__(self):
         super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 8, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(16 * 7 * 7, 32)
-        self.fc2 = nn.Linear(32, 10)
+        self.conv1 = nn.Conv2d(1, 8, kernel_size=5, padding=2)
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=5, padding=2)
+        self.fc1 = nn.Linear(16 * 7 * 7, 16)
+        self.fc2 = nn.Linear(16, 10)
         self.pool = nn.MaxPool2d(2, 2)
         self.relu = nn.ReLU()
 
